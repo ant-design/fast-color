9890ddbc6a423fb57141ef718c5617d605a19c2e
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-18 16:05:36 +0800

    getLuminance

--- src/FastColor.ts
+++ src/FastColor.ts
@@ -117,6 +117,39 @@ export class FastColor {
     return this.a;
   }
 
+  /**
+   * Returns the perceived luminance of a color, from 0-1.
+   * @see http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+   */
+  getLuminance(): number {
+    let R;
+    let G;
+    let B;
+    const RsRGB = this.r / 255;
+    const GsRGB = this.g / 255;
+    const BsRGB = this.b / 255;
+
+    if (RsRGB <= 0.03928) {
+      R = RsRGB / 12.92;
+    } else {
+      R = Math.pow((RsRGB + 0.055) / 1.055, 2.4);
+    }
+
+    if (GsRGB <= 0.03928) {
+      G = GsRGB / 12.92;
+    } else {
+      G = Math.pow((GsRGB + 0.055) / 1.055, 2.4);
+    }
+
+    if (BsRGB <= 0.03928) {
+      B = BsRGB / 12.92;
+    } else {
+      B = Math.pow((BsRGB + 0.055) / 1.055, 2.4);
+    }
+
+    return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+  }
+
   getHue() {
     if (typeof this._h === 'undefined') {
       if (this.max === this.min) {
