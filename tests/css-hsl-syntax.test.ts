9fb4e75d6f1898a28790f23b5122289e93501423
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-21 16:25:21 +0800

    test hsl

--- tests/css-hsl-syntax.test.ts
+++ tests/css-hsl-syntax.test.ts
@@ -1,142 +1,134 @@
 import { FastColor } from '../src';
 
-// Support CSS hsl() and rgba() syntax with absolute values
+// Support CSS hsl() and hsla() syntax with absolute values
 // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/hsl
-describe('css rgb() syntax', () => {
+describe('css hsl() syntax', () => {
   describe('new space-separated syntax', () => {
-    it('parse number (0-255)', () => {
-      expect(new FastColor('rgb(255 255 255)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+    it('without units', () => {
+      expect(new FastColor('hsl(270 60 40)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 1,
       });
-    });
 
-    it('parse number alpha (0-1)', () => {
-      expect(new FastColor('rgb(255 255 255 / 0.2)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / 0.2)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgb(255 255 255 / .2)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / .2)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgb(255 255 255 / 0.233)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / 0.233)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
 
-      expect(new FastColor('rgb(255 255 255 / .233)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / .233)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
     });
 
-    it('parse percentage (0-100%)', () => {
-      expect(new FastColor('rgb(100% 100% 100%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+    it('with units', () => {
+      expect(new FastColor('hsl(270 60 40)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 1,
       });
 
-      expect(new FastColor('rgb(100% 100% 100%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 1,
       });
-    });
 
-    it('parse percentage alpha (0-1)', () => {
-      expect(new FastColor('rgb(100% 100% 100% / 20%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / 20%)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgb(100% 100% 100% / 23.3%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsl(270 60 40 / 23.3%)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
     });
   });
 
   describe('old comma-separated syntax', () => {
-    it('parse number (0-255)', () => {
-      expect(new FastColor('rgb(255, 255, 255)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+    it('without units', () => {
+      expect(new FastColor('hsl(270, 60, 40)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 1,
       });
-    });
 
-    it('parse number alpha (0-1)', () => {
-      expect(new FastColor('rgba(255, 255, 255, 0.2)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270, 60, 40, 0.2)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgba(255, 255, 255, .2)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270, 60, 40, .2)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgba(255, 255, 255, 0.233)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270, 60, 40, 0.233)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
 
-      expect(new FastColor('rgba(255, 255, 255, .233)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270, 60, 40, .233)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
     });
 
-    it('parse percentage (0-100%)', () => {
-      expect(new FastColor('rgb(100%, 100%, 100%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+    it('with units', () => {
+      expect(new FastColor('hsl(270deg, 60%, 40%)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 1,
       });
-    });
 
-    it('parse percentage alpha (0-1)', () => {
-      expect(new FastColor('rgba(100%, 100%, 100%, 20%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270deg, 60%, 40%, 20%)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.2,
       });
 
-      expect(new FastColor('rgba(100%, 100%, 100%, 23.3%)').toRgb()).toEqual({
-        r: 255,
-        g: 255,
-        b: 255,
+      expect(new FastColor('hsla(270deg, 60%, 40%, 23.3%)').toRgb()).toEqual({
+        r: 102,
+        g: 41,
+        b: 163,
         a: 0.233,
       });
     });
