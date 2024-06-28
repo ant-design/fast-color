20e7afee195304aaa923f39d90a9f87c22876b51
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2024-06-28 17:23:07 +0800

    test

--- tests/ctrl-tinycolor/rgbToHsv.test.ts
+++ tests/ctrl-tinycolor/rgbToHsv.test.ts
@@ -1,19 +1,13 @@
 import { rgbToHsv } from '@ctrl/tinycolor';
+import type { HSV } from '../../src';
 import { FastColor } from '../../src';
 
 test('rgbToHsv alternative', () => {
-  expect(rgbToHsv(11, 22, 33)).toEqual({
-    h: 0.5833333333333334,
-    s: 0.6666666666666667,
-    v: 0.12941176470588237,
-  });
-
-  expect(new FastColor({ r: 11, g: 22, b: 33 }).toHsv()).toEqual({
-    h: 360 * 0.5833333333333334,
-    s: 0.6666666666666666,
-    v: 0.12941176470588237,
-    a: 1,
-  });
-
-  expect(new FastColor('#66ccff').toHsv()).toBe('');
+  const r = 102,
+    g = 204,
+    b = 255;
+  const hsv: HSV = rgbToHsv(r, g, b);
+  hsv.h = Math.round(hsv.h * 360);
+  hsv.a = 1;
+  expect(new FastColor({ r, g, b }).toHsv()).toEqual(hsv);
 });
