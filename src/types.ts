65641d8ac6d61145538cab695bc76a757d839328
二货爱吃白萝卜 <smith3816@gmail.com>
2024-07-11 17:31:03 +0800

    chore: a is not optional (#23)

--- src/types.ts
+++ src/types.ts
@@ -2,21 +2,21 @@ export interface RGB {
   r: number;
   g: number;
   b: number;
-  a?: number;
+  a: number;
 }
 
 export interface HSL {
   h: number;
   s: number;
   l: number;
-  a?: number;
+  a: number;
 }
 
 export interface HSV {
   h: number;
   s: number;
   v: number;
-  a?: number;
+  a: number;
 }
 
 export type ColorInput = string | RGB | HSL | HSV;
