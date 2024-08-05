0abd4b099995917fd3b9a19bff52f38d5303a682
wuxh <wxh1220@gmail.com>
2024-08-05 19:28:20 +0800

    Revert "fix(build): ignore compile targets"
    This reverts commit 8b3e424830aa2dfe2a192aca77c428181f60755c.

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
+  targets: {
+    chrome: 74, // es2015, aligned with @ctrl/tinycolor
+  },
 });
