8b3e424830aa2dfe2a192aca77c428181f60755c
wuxh <wxh1220@gmail.com>
2024-08-05 19:19:03 +0800

    fix(build): ignore compile targets

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,7 +2,4 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
-  targets: {
-    chrome: 74, // es2015, aligned with @ctrl/tinycolor
-  },
 });
