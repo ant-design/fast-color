4f95aaa12b954268de8bdd8a3eb0ad1f79c771b7
二货机器人 <smith3816@gmail.com>
2025-03-21 03:14:12 +0800

    chore: bump father version

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,10 +2,4 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   plugins: ['@rc-component/father-plugin'],
-  targets: {
-    // It's annoying ts will add prop def in class.
-    // We have to use low version to compatible with this
-    // Since some user not upgrade their webpack.
-    chrome: 73,
-  },
 });
