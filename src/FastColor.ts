39d89156a58b5438397e9200bba2274db9fff067
二货爱吃白萝卜 <smith3816@gmail.com>
2025-03-21 08:16:52 +0800

    feat: support preset color (#85)
    * chore: bump father version
    
    * chore: preset color
    
    * test: update test case
    
    * test: update test case
    
    * chore: bumdle size

--- src/FastColor.ts
+++ src/FastColor.ts
@@ -1,3 +1,4 @@
+import presetColors from './presetColors';
 import type { ColorInput, HSL, HSV, OptionalA, RGB } from './types';
 
 type Constructor<T> = new (...args: any[]) => T;
@@ -126,6 +127,15 @@ export class FastColor {
         this.fromHslString(trimStr);
       } else if (matchPrefix('hsv') || matchPrefix('hsb')) {
         this.fromHsvString(trimStr);
+      } else {
+        // From preset color
+        const presetColor = presetColors[trimStr.toLowerCase()];
+        if (presetColor) {
+          this.fromHexString(
+            // Convert 36 hex to 16 hex
+            parseInt(presetColor, 36).toString(16).padStart(6, '0'),
+          );
+        }
       }
     } else if (input instanceof FastColor) {
       this.r = input.r;
